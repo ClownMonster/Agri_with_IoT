d802e1c174fd51ee312a6f8be5d7b5e9775cc9db
Clown Monster <mk6386223@gmail.com>
2020-07-01 19:41:20 +0530

    backed model

---
+++ cloudTestData.py
@@ -0,0 +1,15 @@
+
+
+data = {
+    'Air Temp':20.13017482,
+    'Soil Humidity':81.60487287,
+    'Soil pH':7.628472891,
+    'Rainfall':262.7173405
+}
+
+data1= {
+    'Air Temp':25.586226,
+    'Soil Humidity':81.61082872,
+    'Soil pH':5.721130543,
+    'Rainfall':112.7173405
+}
